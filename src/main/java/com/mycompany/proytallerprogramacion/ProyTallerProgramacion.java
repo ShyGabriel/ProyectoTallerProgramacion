f2394ab949d4c3042a955cfd415aef34c3a18b56
hegam <hegam@192.168.0.120>
2023-11-26 20:35:27 -0500

    prueba

---
+++ src/main/java/com/mycompany/proytallerprogramacion/ProyTallerProgramacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proytallerprogramacion;
+
+/**
+ *
+ * @author hegam
+ */
+public class ProyTallerProgramacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
