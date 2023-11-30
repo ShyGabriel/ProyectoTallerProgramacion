d6c6c30f5171e09ea51dbd5a0598af9456963055
hegam <hegam@192.168.0.120>
2023-11-30 17:19:06 -0500

    modificaciones

--- src/main/java/com/mycompany/proytallerprogramacion/Transacciones.java
+++ src/main/java/com/mycompany/proytallerprogramacion/Transacciones.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proytallerprogramacion;
 
 import java.util.Scanner;
 
 /**
  *
- * @author hegam
+ * @author Henry Marina
  */
 public class Transacciones {
     //Creamis un método para buscar el número de cuenta
